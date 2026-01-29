8a1291bd45a0ebcdb6e3cbfe1df4ef872d3cdbea
igorskyflyer <igor.dvlpr@gmail.com>
2026-01-29 01:23:51 +0100

    Add UserScript

---
+++ script.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         Twitch - Mute homepage
+// @namespace    https://greasyfork.org/users/1002054-igorskyflyer
+// @version      v1.0.0
+// @description  🔇 Mutes auto-playing videos on Twitch's homepage. 📺
+// @author       igorskyflyer
+// @date         2026-01-29
+// @match        https://www.twitch.tv/
+// @match        https://twitch.tv/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
+// @run-at       document-idle
+// @grant        none
+// @license      GPL-3.0-or-later
+// @compatible   chrome
+// @compatible   firefox
+// @compatible   opera
+// @compatible   safari
+// @compatible   edge
+// @homepageURL  https://github.com/igorskyflyer/userscript-twitch-mute-homepage
+// @supportURL   https://greasyfork.org/users/1002054-igorskyflyer
+// ==/UserScript==
+
+// @ts-nocheck
+
+;(() => {
+  const MAX_ATTEMPTS = 5
+  const POLL_INTERVAL = 2000
+
+  let attemptsLeft = MAX_ATTEMPTS
+  let foundPlayer = false
+
+  window.addEventListener('load', () => {
+    function findPlayer() {
+      const player = document.querySelector('video[playsinline]')
+
+      if (!player) {
+        return
+      }
+
+      player.addEventListener('playing', () => {
+        player.volume = 0
+      })
+
+      foundPlayer = true
+    }
+
+    const timer = setInterval(() => {
+      findPlayer()
+      attemptsLeft--
+
+      if (foundPlayer || attemptsLeft <= 0) {
+        clearInterval(timer)
+
+        if (!foundPlayer) {
+          // biome-ignore lint/suspicious/noConsole: Needed for debug purposes.
+          console.error(
+            'Could not find the player. Please visit https://greasyfork.org/users/1002054-igorskyflyer and report the issue.'
+          )
+        }
+      }
+    }, POLL_INTERVAL)
+  })
+})()
